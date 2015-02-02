c79ff03f65a4baf5a2cb66b0d08c51fb4f8bbbeb
FranciscoPorcel <pacops32@gmail.com>
2015-02-02 20:47:44 +0100

    Añadidas clases para el manejo de formulario, certificados y actualizado el test #8 #14 #2

--- pruebadriveiv.py
+++ pruebadriveiv.py
@@ -61,33 +61,62 @@ class MainPage(webapp2.RequestHandler):
 		template = JINJA_ENVIRONMENT.get_template('templates/MAIN_PAGE_HTML_BOOT.html')
 		self.response.write(template.render(template_values))
 
-#Manejador que lee los datos del formulario y realiza la inserción
+#Esta clase se encarga de gestionar los datos introducidos en el formulario de registro de nuevos eventos (introduce en calendar y en hoja de calculo)
 class Guestbook(webapp2.RequestHandler):
-	def post(self):
-		client = gdata.spreadsheet.service.SpreadsheetsService()
-		client.debug = True
-		client.email = email
-		client.password = password
-		client.source = 'test client'
-		client.ProgrammaticLogin()
-	    
-		rows = []
-		rows.append({'id':cgi.escape(self.request.get('nombre')),'title':cgi.escape(self.request.get('descripcion'))})  
+   @decorator.oauth_required
+   def post(self):
+	client = gdata.spreadsheet.service.SpreadsheetsService()
+	client.debug = True
+	client.email = email
+	client.password = password
+	client.source = 'test client'
+	client.ProgrammaticLogin()
+	# Se comprueba el valor de los radio button
+	if self.request.get('inscripcion') == "inscripcion_si":
+		inscripcion = "Si"
+	else:
+		inscripcion = "No"
 
-		for row in rows:
-			try:
-				client.InsertRow(row, spreadsheet_key, worksheet_id)
-			except Exception as e:
-				print e
-	    
-	    #return
-		self.response.write(URL_SPREADSHEET_HTML)
+	if self.request.get('diploma') == "diploma_si":
+		diploma = "Si"
+	else:
+		diploma = "No"
+
+	if self.request.get('tipo') == "tipo_osl":
+		tipo = "OSL"
+	elif self.request.get('tipo') == "tipo_compratido":
+		tipo = "tipo_compratido"
+	else:
+		tipo = "tipo_fuera"
+
+	#Se crea una nueva fila y se inserta en la hoja de calculo    
+	rows = []
+	tiempo = time.strftime("%c")
+	rows.append({'titulo':cgi.escape(self.request.get('InputName')),'ponente':cgi.escape(self.request.get('InputPonente')),'fecha':tiempo,'inscripcion':inscripcion,'diploma':diploma,'tipo':tipo,'descripcion':cgi.escape(self.request.get('InputDescripcion')),'dia':cgi.escape(self.request.get('dia')),'mes':cgi.escape(self.request.get('mes')),'anio':cgi.escape(self.request.get('anio')),'hora':cgi.escape(self.request.get('hora')),'minuto':cgi.escape(self.request.get('minuto'))})
+	for row in rows:
+		try:
+			client.InsertRow(row, spreadsheet_key, worksheet_id)
+		except Exception as e:
+			print e
+	
+	# Se introduce el evento en el calendario
+	fecha_evento = ""+self.request.get('anio')+"-"+self.request.get('mes')+"-"+self.request.get('dia')
+	http = decorator.http()
+	event = {"end": {"date": fecha_evento},"start": {"date": fecha_evento},"summary": cgi.escape(self.request.get('InputName')),"description": cgi.escape(self.request.get('InputDescripcion'))}
+	request = service.events().insert(calendarId='proyectoivosl@gmail.com', body=event).execute(http=http)
+	#Se visualiza la tabla de los eventos creados
+	template_values = {}
+	template = JINJA_ENVIRONMENT.get_template('templates/hoja_calculo.html')
+	self.response.write(template.render(template_values))
 
 #Manejador para ejecutar el test	    
 class Test(webapp2.RequestHandler):
 	def get(self):
 		Inserta('OK')
-		self.response.write(URL_SPREADSHEET_HTML)
+		#Se visualiza la tabla de los eventos creados
+		template_values = {}
+		template = JINJA_ENVIRONMENT.get_template('templates/hoja_calculo.html')
+		self.response.write(template.render(template_values))
 	#Funcion que se encarga de realizar la insercion. En caso de que no la realice de forma correcta el valor de la variable valor sera "error". Por otro lado si se realiza con exito, conservara su valor, que es "OK"
 	def Inserta(self,valor):
 		client = gdata.spreadsheet.service.SpreadsheetsService()
@@ -95,11 +124,10 @@ class Test(webapp2.RequestHandler):
 		client.email = email
 		client.password = password
 		client.source = 'test client'
-		client.ProgrammaticLogin()
-	    
+		client.ProgrammaticLogin()	    
 		rows = []
 		tiempo = time.strftime("%c")
-		rows.append({'id':'Prueba','title':tiempo})	    
+		rows.append({'titulo': 'Prueba','ponente': "Ponente de prueba",'fecha':tiempo,'inscripcion':'Inscripcion de prueba','diploma':'Diploma de prueba','tipo':'Tipo de prueba','descripcion':'Descripcion','dia':'Dia','mes':'Mes','anio':'Anio','hora':'Hora','minuto':'Minuto'})
 
 		for row in rows:
 			try:
@@ -110,6 +138,13 @@ class Test(webapp2.RequestHandler):
 		
 		return valor
 
+# Devuelve el html que contiene el formulario de insercion de un nuevo evento
+class Formulario(webapp2.RequestHandler):
+    def get(self):
+	template_values = {}
+	template = JINJA_ENVIRONMENT.get_template('templates/formulario.html')
+	self.response.write(template.render(template_values))
+
 # Devuelve el html que contiene la hoja de calculo
 class Hoja(webapp2.RequestHandler):
     def get(self):
@@ -117,13 +152,30 @@ class Hoja(webapp2.RequestHandler):
 		template = JINJA_ENVIRONMENT.get_template('templates/hoja_calculo.html')
 		self.response.write(template.render(template_values))
 
+#Devuelve un certificado personalizado para el ponente del evento que se esta creando
+class Certificado(webapp2.RequestHandler):
+    def post(self):
+	ponente = self.request.get('InputPonente')
+	evento = self.request.get('InputName')
+	dia = self.request.get('dia')
+	mes = self.request.get('mes')
+	anio = self.request.get('anio')
+	hora = self.request.get('hora')
+	minuto = self.request.get('minuto')
+	descripcion = self.request.get('InputDescripcion')
+	template_values = {'ponente': ponente, 'evento': evento, 'dia': dia, 'mes': mes, 'anio': anio, 'hora': hora, 'minuto': minuto, 'descripcion': descripcion}
+	template = JINJA_ENVIRONMENT.get_template('templates/certificado.html')
+	self.response.write(template.render(template_values))
+
 #Manejo de las URLs
 application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/sign', Guestbook),
+    ('/formulario', Formulario),
     ('/test', Test),
-	('/hoja', Hoja),
-	(decorator.callback_path, decorator.callback_handler())
+    ('/certificado', Certificado),
+    ('/hoja', Hoja),
+    (decorator.callback_path, decorator.callback_handler())
 ], debug=True)
 
 JINJA_ENVIRONMENT = jinja2.Environment(
